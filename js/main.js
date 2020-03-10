fa6200a49e50e33728255aea1f874275c060b9b6
Jessica <jessica.schwabegger@hotmail.com>
2020-03-10 12:56:06 +0100

    Counter
    Created a counter that keeps track of turns.
    Created a timer that tells you how long it took to finish the game.

--- js/main.js
+++ js/main.js
@@ -4,16 +4,15 @@ function showCards () {
 
     const cards = document.querySelectorAll(".parent article");
     let cardsToCheck = [];
-    // let pairs = [];
     let noMatch = [];
-
+    let pairs = [];
+    let turns = 0;
 
     for(let card of cards) {
         const icon = card.querySelector(".material-icons");
 
         card.addEventListener("click", () => {
-
-            console.log(noMatch);
+            let timer = setTimeout(1000);
 
             if(noMatch.length >= 2) {
                 for(let item of noMatch) {
@@ -33,19 +32,60 @@ function showCards () {
             if (cardsToCheck.length == 2) {
                 if (cardsToCheck[0].children[0].innerText == cardsToCheck[1].children[0].innerText) {
                     console.log("jaaaa");
-                    // match();
+                    pairs.push(cardsToCheck[0], cardsToCheck[1]);
 
                 }else {
                     console.log("nope");
                     noMatch.push(cardsToCheck[0], cardsToCheck[1]);
-
             }
+            turns++;
+            console.log(turns);
             cardsToCheck = [];
-           
+            }
+
+
+            if (pairs.length == cards.length) {
+                console.log("WInner");
+                alertWinner();
+                myTurns(turns);
+                clearTimeout(timer);
+                myTime(timer);
             }
 
         })
+
     }
+}
+
+function myTurns(turns){
+    const winnerSection = document.querySelector(".winner");
+    const text = document.createElement("p");
+    text.innerText = "You made " + turns + " moves!";
+    winnerSection.append(text);
+
+
+}
+function myTime(time){
+    const winnerSection = document.querySelector(".winner");
+    const text = document.createElement("p");
+    text.innerText = "Your time was " + time + " seconds!";
+    winnerSection.append(text);
+}
+
+function alertWinner() {
+    const div = document.querySelector(".root");
+    const winnerSection = document.createElement("section");
+    winnerSection.classList.add("winner");
+    const title = document.createElement("h2");
+    const titleSection = document.createElement("section");
+    titleSection.style.display = "flex";
+    titleSection.style.justifyContent = "center";
+
+    title.innerText = "You are the winner!";
+    titleSection.append(title);
+    winnerSection.append(titleSection);
+    div.append(winnerSection);
+
 }
 
 function randomCards () {
@@ -57,13 +97,11 @@ function randomCards () {
 }
 
 
-function noMatch(cards) {
-    // cards[0].classList.add("hide");
-    // cards[1].classList.add("hide");
-}
-
 showCards();
+//alertWinner();
 
 
 //Jämför två kort. noMatch? = Ge korten klassen hide och lägg i ny array.
 // Match? = Lägg korten i ny array "Pairs".
+//High-Score, 5 snabbaste tiderna? lägg in på local storage.
+
